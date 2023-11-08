16b4ec7800bcddc9881a36cf66bc9fe56e797859
Alonzo Machiraju <machthree@gmail.com>
2023-11-07 16:48:29 -0800

    Restore fastserver.py

---
+++ fastapiserver.py
@@ -0,0 +1,43 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse, StreamingResponse
+import io
+from PIL import Image
+
+app = FastAPI()
+origins = [
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class SamplePost(BaseModel):
+    name: str
+    age: int
+    file_path: str
+@app.post('/')
+async def body_method(sample_post: SamplePost):
+    # return "hello " +  sample_post.name
+    out_string = f'Hello {sample_post.name}, your age is {sample_post.age} and file path is {sample_post.file_path}'
+    return out_string
+@app.get("/basic_image", responses={200: {
+    "content": {"image/png": {}},
+    "description": "Return an image."
+}})
+async def basic_image():
+    image_path = 'F:/FastAPITest/bb.png'
+
+    try:
+         img = Image.open(image_path)
+    except FileNotFoundError:
+         raise HTTPException(status_code=404, detail="Image not found")
+
+    img_byte_arr = io.BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)  
+    return StreamingResponse(img_byte_arr, media_type="image/png")
